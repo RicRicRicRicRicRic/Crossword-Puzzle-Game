08dbc393c37bf11a3be43f728f733ee1fb45896f
Estremadura, Ric Michael <ricmichaele@gmail.com>
2025-03-18 00:39:47 +0800

    frontend

---
+++ frontend/src/components/Home.vue
@@ -0,0 +1,21 @@
+//components/Home.vue
+<script>
+    export default {
+        name: 'Home',
+        mounted() {
+            document.title = 'Home Page'; // Dynamically set the title
+        }
+    };
+</script>
+
+<template>
+    <div>
+        <p>test</p>
+    </div>
+</template>
+
+
+
+<style  scoped>
+
+</style>
