0cff572081990133997271d9a06cd6235c9b8bd1
Estremadura, Ric Michael <ricmichaele@gmail.com>
2025-03-20 00:28:51 +0800

    authentication gaurd
    if in dashboard page but not authorized, redirect to login

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -2,26 +2,40 @@ import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../components/HomePage/Home.vue'
 import Authentication from '../components/authentication/Auth_page.vue'
 import Dashboard from '@/components/Dashboard/Dashboard_Page.vue'
+import store from '@/store/store' 
+
+const routes = [
+  {
+    path: '/',
+    name: 'Home',
+    component: Home,
+  },
+  {
+    path: '/Login_or_Register',
+    name: 'Auth_page',
+    component: Authentication,
+  },
+  {
+    path: '/Dashboard',
+    name: 'DashboardPage',
+    component: Dashboard,
+    meta: { requiresAuth: true } // This route requires authentication
+  },
+]
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'Home',
-      component: Home,
-    },
-    {
-      path: '/Login_or_Register',
-      name: 'Auth_page',
-      component: Authentication,
-    },
-    {
-      path: '/Dashboard',
-      name: 'DashboardPage',
-      component:  Dashboard,
-    },
-  ],
+  routes,
+})
+
+
+router.beforeEach((to, from, next) => {
+  if (to.meta.requiresAuth && !store.state.user) {
+
+    next({ name: 'Auth_page' })
+  } else {
+    next()
+  }
 })
 
 export default router
